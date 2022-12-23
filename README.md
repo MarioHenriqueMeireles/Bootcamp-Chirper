32f6d9717a1f91fbfd48c13dd232533f5e5e8a87
MarioHenriqueMeireles <mario.aquino.dev@gmail.com>
2022-12-23 10:30:59 -0300

    First and last commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Bootcamp Chirper
+
+This project is just a training in Laravel 9 to practice the framework's new features.
+
+It was made using Blade and Sqlite.
+
+I recommend it!
+
+### See more in <a href='https://bootcamp.laravel.com/'>https://bootcamp.laravel.com/ and try it yourself!
+
+## Do you want play? So, Do it!
+
+- Download the project
+- php artisan migrate
+- php artisan serve
+- Open in a Browser
+- Register a new user
+- Play a lite
---
+++ config/services.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme' => 'https',
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+    'facebook' => [
+        'client_id' => '1756197828107503',
+        'client_secret' => '8072aee605755a787cdd458318840241',
+        'redirect' => env('FACEBOOK_REDIRECT_URI')
+    ],
+];
